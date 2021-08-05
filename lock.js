457823000f312690e66a705cf9fd59a34e44607f
Nieky Allen <nieky.allen@gmail.com>
2021-08-04 23:47:49 -0500

    Update lock.js

--- lock.js
+++ lock.js
@@ -1,3 +1,12 @@
 
 
-Module.register('autolock', {});
+Module.register('autolock', {
+    start: function () {
+		    Log.info("Starting module: " + this.name);
+    },
+  getDom: function () {
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = '<p style="color: red;">hello there</p>';
+    return wrapper;
+  }
+});
